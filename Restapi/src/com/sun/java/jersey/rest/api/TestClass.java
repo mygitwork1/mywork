905b14a341efbd5bac60fed7c394ef74a115e0fe
subbuwork <subbu.varsith1@gmail.com>
2016-04-07 11:14:55 -0400

    added get and path annotations

--- Restapi/src/com/sun/java/jersey/rest/api/TestClass.java
+++ Restapi/src/com/sun/java/jersey/rest/api/TestClass.java
@@ -5,11 +5,17 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/test")
 public class TestClass {
-	
+	private static final String version = "0.1.12";
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String returnVal(){
 		return"<p>Welcome to REST world you will see new world !!!!</p>";
 	}
-
+	 
+    @Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String getVersion(){
+		return"<p>Version Number:::</p>"+version;
+	}
 }
