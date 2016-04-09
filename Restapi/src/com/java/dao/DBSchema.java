78ba8c9b0306dbb999b5741471f57b0a4d5b86c5
subbuwork <subbu.varsith1@gmail.com>
2016-04-09 15:26:34 -0400

    POST verb functionality added

--- Restapi/src/com/java/dao/DBSchema.java
+++ Restapi/src/com/java/dao/DBSchema.java
@@ -1,10 +1,18 @@
 package com.java.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jettison.json.JSONArray;
 
 import com.java.utils.ToJSON;
@@ -37,6 +45,70 @@ public class DBSchema extends DAO {
 			}
 		}
 		return array;
-			}
+	}
+	
+
+	/**
+	 * This method will insert a record into the STUDENT table. 
+	 * 
+	 * Note: there is no validation being done... if this was a real project you
+	 * must do validation here!
+	 * 
+	 * @param SID
+	 * @param FNAME
+	 * @param LNAME
+	 * @param ADDRESS
+	 * @param DOJ
+	 * @param DOB
+	 * @param COURSE
+	 * @param GRADE
+	 * @return integer 200 for success, 500 for error
+	 * @throws Exception,SQLException 
+	 * @throws Exception
+	 */
+
+	public Integer insertStudent(Integer sID, 
+			                     String fName,
+			                     String lName,
+			                     String address,
+			                     Date doj,
+			                     Date dob,
+			                     String cource,
+			                     String grade) throws Exception,SQLException {
+     PreparedStatement preparedStatement = null;
+     Connection connection               = null;
+     
+     try {
+    	 
+    	 /*
+			 * If this was a real application, you should do data validation here
+			 * before starting to insert data into the database.
+			 */
+    	 
+    	 connection = oracleStudentsConnection();
+    	 preparedStatement = connection.prepareStatement("inset into student"+
+    	                                                 "(SID,FNAME,LNAME,ADDRESS,DOB,DOJ,COURCE,GRADE)"+
+    			                                         "VALUES(?,?,?,?,?,?,?,?)");
+    	 preparedStatement.setInt(1,sID);
+    	 preparedStatement.setString(2,fName);
+    	 preparedStatement.setString(3,lName);
+    	 preparedStatement.setString(4,address);
+    	 preparedStatement.setDate(5,dob);
+    	 preparedStatement.setDate(6, doj);
+    	 preparedStatement.setString(7,cource);
+    	 preparedStatement.setString(8,grade);
+    	 
+    	 preparedStatement.executeUpdate();
+		
+	} catch (Exception e) {
+		e.printStackTrace();
+		return 500;// error
+	}finally{
+		if(connection != null){
+			connection.close();
+		}
+	}
+		return 200;// success
+	}
 
 }
---
+++ Restapi/src/com/java/dao/Studentdao.java
@@ -0,0 +1,73 @@
+package com.java.dao;
+
+import java.sql.Date;
+
+public class Studentdao {
+	
+	public Integer sID;
+	public String  fName;
+	public String  lName;
+	public String  address;
+	public Date  doj;
+	public Date  dob;
+	public String  course;
+	public String  grade;
+	
+	
+	public Studentdao(){
+		
+	}
+	
+	
+	public Integer getsID() {
+		return sID;
+	}
+	public void setsID(Integer sID) {
+		this.sID = sID;
+	}
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Date getDoj() {
+		return doj;
+	}
+	public void setDoj(Date doj) {
+		this.doj = doj;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getCource() {
+		return course;
+	}
+	public void setCource(String cource) {
+		this.course = cource;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	
+	
+
+}
