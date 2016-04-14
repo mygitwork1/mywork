4c856e944f56a71a8587e2fcb35afc4c832074ce
subbuwork <subbu.varsith1@gmail.com>
2016-04-13 22:00:27 -0400

    data base functionality added

---
+++ Spring/src/com/java/spring/core/Foo.java
@@ -0,0 +1,13 @@
+package com.java.spring.core;
+
+public class Foo {
+	
+	public void Foo(){
+		System.out.println("this is FOO class:::");
+	}
+	
+	public void test(){
+		System.out.println("Test method:::::");
+	}
+
+}
--- Spring/src/com/java/spring/core/Service.java
+++ Spring/src/com/java/spring/core/Service.java
@@ -1,12 +1,18 @@
 package com.java.spring.core;
 
+
 public class Service {
 	String name;
-	public Service(String name){
+	Foo foo;
+	
+	//@ConstructorProperties({"name","foo"})
+	public Service(String name,Foo foo1){
 		this.name=name;
+		foo = foo1;
 	}
 	public void test(){
 		System.out.println("Welcome to spring world....."+name);
+		foo.test();
 	}
 
 }
